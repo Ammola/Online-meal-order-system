9a9653f070532d29d94aca72c7ebe69eb3254705
Alanoud95 <AlanoudAlshabib@gmail.com>
2021-01-28 12:13:27 +0300

    Edit Meal.java

--- src/main/java/com/ga/restaurantapp/model/Meal.java
+++ src/main/java/com/ga/restaurantapp/model/Meal.java
@@ -2,12 +2,8 @@ package com.ga.restaurantapp.model;
 
 import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 
 
 @Entity
@@ -21,8 +17,44 @@ public class Meal {
 	@Column(length = 50)
 	private int MealPrice;
 	
-	@ManyToMany(mappedBy="OrderMeals")
-	private Set<Order> orders;
+//	@ManyToMany
+//	@JoinTable(name = "OrderMeals", joinColumns = { @JoinColumn(name = "MealId")}
+//	, inverseJoinColumns = { @JoinColumn(name ="OrderId")})
+	
+//	private Set<Order> orders;
+
+	public int getMealId() {
+		return MealId;
+	}
+
+	public void setMealId(int mealId) {
+		MealId = mealId;
+	}
+
+	public String getMealName() {
+		return MealName;
+	}
+
+	public void setMealName(String mealName) {
+		MealName = mealName;
+	}
+
+	public int getMealPrice() {
+		return MealPrice;
+	}
+
+	public void setMealPrice(int mealPrice) {
+		MealPrice = mealPrice;
+	}
+
+//	public Set<Order> getOrders() {
+//		return orders;
+//	}
+//
+//	public void setOrders(Set<Order> orders) {
+//		this.orders = orders;
+//	}
+	
 
 }
 	
