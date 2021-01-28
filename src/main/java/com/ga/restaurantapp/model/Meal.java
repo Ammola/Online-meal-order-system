c2ef5463316e4d91027cdbdb92e0c16bc8fd1443
Alanoud95 <AlanoudAlshabib@gmail.com>
2021-01-28 10:19:34 +0300

    Meal model

---
+++ src/main/java/com/ga/restaurantapp/model/Meal.java
@@ -0,0 +1,31 @@
+package com.ga.restaurantapp.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.ga.blogapp.model.Author;
+
+@Entity
+@Table(name="Meal")
+public class Meal {
+	
+	@Id
+	@GeneratedValue
+	private int MealId;
+	private String MealName;
+	@Column(length = 50)
+	private int MealPrice;
+	
+	@ManyToMany(mappedBy="OrderMeals")
+	private Set<Order> orders;
+
+	
+	
+
+}
