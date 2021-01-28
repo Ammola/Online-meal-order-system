5097464b8857d42878a9804adf9e8375eab8d430
libredev-rayan <rian.h04@gmail.com>
2021-01-28 17:21:08 +0300

    test

--- src/main/java/com/ga/restaurantapp/model/Meal.java
+++ src/main/java/com/ga/restaurantapp/model/Meal.java
@@ -1,28 +1,29 @@
 package com.ga.restaurantapp.model;
-//
-//import java.util.Set;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.Id;
-//import javax.persistence.ManyToMany;
-//import javax.persistence.Table;
-//
-//
-//@Entity
-//@Table(name="Meal")
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="Meal")
 public class Meal {
 	
-//	@Id
-//	@GeneratedValue
-//	private int MealId;
-//	private String MealName;
-//	@Column(length = 50)
-//	private int MealPrice;
-//	
-//	@ManyToMany(mappedBy="OrderMeals")
-//	private Set<Order> orders;
+	@Id
+	@GeneratedValue
+	private int MealId;
+	private String MealName;
+	@Column(length = 50)
+	private int MealPrice;
+	
+	@ManyToMany(mappedBy="OrderMeals")
+	private Set<Order> orders;
 
 }
 	
