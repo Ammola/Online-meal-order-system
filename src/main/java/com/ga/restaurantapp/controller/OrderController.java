bcb64a18d9bb424ef93919a21d59d7db8188ef7c
libredev-rayan <rian.h04@gmail.com>
2021-01-28 13:19:01 +0300

    init Order.java

--- src/main/java/com/ga/restaurantapp/controller/OrderController.java
+++ src/main/java/com/ga/restaurantapp/controller/OrderController.java
@@ -1,5 +1,37 @@
 package com.ga.restaurantapp.controller;
 
-public class OrderController {
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+//
+//import com.ga.blogapp.dao.ArticleDao;
+//import com.ga.blogapp.model.Article;
+//import com.ga.restaurant.controller.UserController;
+//import com.ga.restaurant.dao.AuthorDao;
+//
+//@Controller
+public class OrderController {
+//	
+//	@Autowired
+//	private MealDao mealdao;
+//	
+//	@Autowired
+//	private UserController uc;
+//	
+//	@Autowired
+//	HttpServletRequest request;
+//	
+//
+//	@Autowired
+//	private OrderDao dao;
+//	
+//	// HTTP POST REQUEST - Article Add
+//	@PostMapping("/Order/add")
+//	public String addArticle(Order order) {
+//		dao.save(order);
+//		
+//		return "redirect:/order/index";
+//	}
 }
