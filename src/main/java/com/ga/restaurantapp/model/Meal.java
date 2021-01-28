bfb6f3cda743e0ae92af11cf8e71a5fe33617155
Ammola <ooammolaoo@gmail.com>
2021-01-28 17:34:41 +0300

    add order and meal models

--- src/main/java/com/ga/restaurantapp/model/Meal.java
+++ src/main/java/com/ga/restaurantapp/model/Meal.java
@@ -1,8 +1,11 @@
 package com.ga.restaurantapp.model;
 
+import java.time.LocalDateTime;
 import java.util.Set;
-
 import javax.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 
 @Entity
@@ -11,13 +14,22 @@ public class Meal {
 	
 	@Id
 	@GeneratedValue
-	private int MealId;
-	private String MealName;
+	private int mealId;
+	private String mealName;
 	@Column(length = 50)
-	private int MealPrice;
+	private int mealPrice;
 	
-	//@ManyToMany(mappedBy="Order")
-	//private Set<Order> orders;
+	@ManyToMany(mappedBy="meals")
+	private Set<Order> orders;
+	
+	@Column(name="createdAt", nullable = false, updatable = false)
+	@CreationTimestamp
+	private LocalDateTime createAt;
+	
+	@Column(name="updatedat", nullable = false, updatable = true)
+	@UpdateTimestamp
+	private LocalDateTime updateAt;
+
 
 }
 	
--- src/main/java/com/ga/restaurantapp/model/Order.java
+++ src/main/java/com/ga/restaurantapp/model/Order.java
@@ -1,5 +1,42 @@
 package com.ga.restaurantapp.model;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+
+@Entity
+@Table(name="Oerder")
 public class Order {
+	
+	@Id
+	@GeneratedValue
+	private int OrderId;
+	
+	private Date OrderDate;
+	
+	private int quantity;
+	
+	private int price;
+	
+	@ManyToMany
+	@JoinTable(name = "meal_order",
+				joinColumns = { @JoinColumn(name = "order_id") },
+				inverseJoinColumns = { @JoinColumn(name = "meal_id")})
+	private Set<Meal> meals;
+	
+	@Column(name="createdAt", nullable = false, updatable = false)
+	@CreationTimestamp
+	private LocalDateTime createAt;
+	
+	@Column(name="updatedat", nullable = false, updatable = true)
+	@UpdateTimestamp
+	private LocalDateTime updateAt;
+
 
 }
