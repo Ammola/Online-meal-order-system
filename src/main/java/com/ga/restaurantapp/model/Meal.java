d7b9cab4684e0d8d037064ab0e9a0a5ae907453f
libredev-rayan <rian.h04@gmail.com>
2021-01-30 17:03:51 +0300

    meal conflict meal controller

--- src/main/java/com/ga/restaurantapp/model/Meal.java
+++ src/main/java/com/ga/restaurantapp/model/Meal.java
@@ -4,19 +4,12 @@ import java.util.Set;
 import javax.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
-
 @Entity
-
-@Table(name="Meal")
-
-@Table(name="meals")
-
+@Table(name="Meals")
 public class Meal {
 	
 	@Id
 	@GeneratedValue
-
 	private int MealId;
 	private String MealName;
 	@Column(length = 50)
@@ -24,122 +17,55 @@ public class Meal {
 	private int Calories;
 	private String Description;
 	
-	@ManyToMany(mappedBy="meals")
-	private Set<Order> orders;
-	@Column(name="createdAt", nullable = false, updatable = false)
-	@CreationTimestamp
-	private LocalDateTime createAt;
-	private int mealId;
-	
-	@Column(length = 50)
-	private String mealName;
-	private int mealPrice;
-	
 	@ManyToOne
 	@JoinColumn(name="FK_AdmiId")
 	private User admin;
-	
+
 	
 	@ManyToMany(mappedBy="meals")
 	private Set<Order> orders;
-	
 	@Column(name="createdAt", nullable = false, updatable = false)
 	@CreationTimestamp
 	private LocalDateTime createAt;
-	
-
 	@Column(name="updatedat", nullable = false, updatable = true)
 	@UpdateTimestamp
 	private LocalDateTime updateAt;
-
 	public int getMealId() {
-
 		return MealId;
 	}
-
 	public void setMealId(int mealId) {
 		MealId = mealId;
 	}
-
 	public String getMealName() {
 		return MealName;
 	}
-
 	public void setMealName(String mealName) {
 		MealName = mealName;
 	}
-
 	public int getMealPrice() {
 		return MealPrice;
 	}
-
 	public void setMealPrice(int mealPrice) {
 		MealPrice = mealPrice;
-
-		return mealId;
 	}
-
-	public void setMealId(int mealId) {
-		this.mealId = mealId;
-	}
-
-	public String getMealName() {
-		return mealName;
-	}
-
-	public void setMealName(String mealName) {
-		this.mealName = mealName;
-	}
-
-	public int getMealPrice() {
-		return mealPrice;
-	}
-
-	public void setMealPrice(int mealPrice) {
-		this.mealPrice = mealPrice;
-
-	}
-
 	public Set<Order> getOrders() {
 		return orders;
 	}
-
 	public void setOrders(Set<Order> orders) {
 		this.orders = orders;
 	}
-
-<
 	public int getCalories() {
 		return Calories;
 	}
-
 	public void setCalories(int calories) {
 		Calories = calories;
 	}
-
 	public String getDescription() {
 		return Description;
 	}
-
 	public void setDescription(String description) {
 		Description = description;
 	}
 	
-	public LocalDateTime getCreateAt() {
-		return createAt;
-	}
-
-	public void setCreateAt(LocalDateTime createAt) {
-		this.createAt = createAt;
-	}
-
-	public LocalDateTime getUpdateAt() {
-		return updateAt;
-	}
-
-
-	public void setUpdateAt(LocalDateTime updateAt) {
-		this.updateAt = updateAt;
-	}
 }
 	
