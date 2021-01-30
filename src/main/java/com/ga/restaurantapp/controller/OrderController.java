b681ece3b795c74a58b755c777439679fe8c8fd6
libredev-rayan <rian.h04@gmail.com>
2021-01-30 18:34:24 +0300

    some adds in add.jsp

--- src/main/java/com/ga/restaurantapp/controller/OrderController.java
+++ src/main/java/com/ga/restaurantapp/controller/OrderController.java
@@ -1,28 +1,19 @@
 package com.ga.restaurantapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class OrderController {
-//	
-//	@Autowired
-//	private MealDao mealdao;
-//	
-//	@Autowired
-//	private UserController uc;
-//	
-//	@Autowired
-//	HttpServletRequest request;
-//	
-//
-//	@Autowired
-//	private OrderDao dao;
-//	
-//	// HTTP POST REQUEST - Article Add
-//	@PostMapping("/Order/add")
-//	public String addArticle(Order order) {
-//		dao.save(order);
-//		
-//		return "redirect:/order/index";
-//	}
+
+	@GetMapping("/order/add")
+	public ModelAndView addOrder() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("order/add");
+		
+		return mv ;
+	}
+	
 }
--- src/main/webapp/views/order/add.jsp
+++ src/main/webapp/views/order/add.jsp
@@ -1,1 +1,23 @@
 <jsp:include page="../shared/_layout.jsp" />
+
+
+<form action="/blogapp/author/add" method="post">
+	<div>
+		<label>Order ID </label> <input type="text" name="Orderid">
+	</div>
+	<div>
+		<label>Quantity </label> <input type="text" name="Quantity">
+	</div>
+	<div>
+		<label>Price </label> <input type="text" name="Price">
+	</div>
+	<div>
+		<label>OrderDate</label> <input type="date" name="OrderDate">
+	</div>
+	<div>
+		<label>customerId</label> <input type="text" name="customerId">
+	</div>
+	
+	<button type="submit">Submit your Order</button>
+	
+</form>
